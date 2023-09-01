50c2cc64e7372a08f8e595e5da746f9b128ea150
rumkugel13 <rumkugel13@outlook.com>
2023-09-01 20:06:55 +0200

    add request and show functions

--- source/app.d
+++ source/app.d
@@ -2,10 +2,15 @@ import std.stdio;
 
 import url;
 
-void main()
+void main(string[] args)
 {
-    writeln("Hi");
+    string test = "http://example.org";
+    if (args.length > 1)
+    {
+        test = args[1];
+    }
 
-    URL url = new URL("http://example.org");
-    
+    URL url = new URL(test);
+    auto result = url.request();
+    url.show(result[1]);
 }
--- source/url.d
+++ source/url.d
@@ -1,9 +1,14 @@
 module url;
 
-import std.range: split;
-import std.algorithm: canFind;
+import std.range : split;
+import std.algorithm : canFind, startsWith;
+import std.string;
+import std.typecons : tuple, Tuple;
+import std.array : join;
+import std.stdio;
 
-class URL {
+class URL
+{
     public string scheme, host, path;
 
     this(string url)
@@ -15,10 +20,78 @@ class URL {
         url = split[1];
         if (!canFind(url, "/"))
             url ~= "/";
-        
+
         split = url.split("/");
         host = split[0];
         path = "/" ~ split[1];
+    }
+
+    Tuple!(string[string], string) request()
+    {
+        import std.socket;
+
+        auto s = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
+        auto address = new InternetAddress(host, 80);
+        s.connect(address);
+
+        import std.utf;
+
+        s.send(("GET " ~ path ~ " HTTP/1.0\r\n" ~ "Host: " ~ host ~ " \r\n\r\n").toUTF8);
+
+        char[1024] buf;
+        string receivedData;
+        long bytesRead;
+
+        while ((bytesRead = s.receive(buf)) > 0)
+        {
+            receivedData ~= buf[0 .. bytesRead];
+        }
+
+        auto lines = receivedData.splitLines();
+
+        auto statusLine = lines[0];
+        auto splitStatus = statusLine.split();
+        auto ver = splitStatus[0];
+        auto status = splitStatus[1];
+        auto explanation = splitStatus[2];
+        assert(status == "200", status ~ ":" ~ explanation);
+
+        string[string] headers;
+        ulong begin = 0;
+        foreach (i, line; lines[1 .. $])
+        {
+            if (line.length == 0)
+            {
+                begin = i + 1;
+                break;
+            }
+
+            auto splitLine = line.split(":");
+            headers[splitLine[0].toLower()] = splitLine[1].strip();
+        }
+
+        assert("transfer-encoding" !in headers);
+        assert("content-encoding" !in headers);
+
+        auto bod = lines[begin .. $].join;
+        s.close();
+
+        return tuple(headers, bod);
+    }
+
+    void show(string bod)
+    {
+        bool inAngle = false;
+        foreach (character; bod)
+        {
+            if (character == '<')
+                inAngle = true;
+            else if (character == '>')
+                inAngle = false;
+            else if (!inAngle)
+                write(character);
+        }
+        writeln();
     }
 }
 
