b0dc45a2ec22eb98b705bdc25a1876d290739ccc
rumkugel13 <rumkugel13@outlook.com>
2025-05-22 15:07:56 +0200

    workaround for windows args passing

--- source/app.d
+++ source/app.d
@@ -1,6 +1,7 @@
 import std.stdio;
 import url, browser;
 import dlangui;
+import std.getopt;
 
 mixin APP_ENTRY_POINT;
 
@@ -11,9 +12,20 @@ extern (C) int UIAppMain(string[] args) // @suppress(dscanner.style.phobos_namin
     test = "https://browser.engineering/styles.html";
     // test = "http://httpforever.com/";
     // test = "http://localhost:8000/index.html";
-    if (args.length > 1)
+    version(Windows)
     {
-        test = args[1];
+        // for some reason, the arguments are not passed correctly on Windows
+        if (args.length > 2)
+        {
+            test = args[2];
+        }
+    }
+    else
+    {
+        if (args.length > 1)
+        {
+            test = args[1];
+        }
     }
 
     URL url = new URL(test);
